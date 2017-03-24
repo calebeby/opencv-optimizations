8b83cfc489a14fd9c8e0423149076df87ea61bf4
Caleb Eby <caleb.eby01@gmail.com>
2017-03-23 21:27:34 -0700

    Remove prints

--- main.py
+++ main.py
@@ -66,10 +66,8 @@ class LiftFinder:
 lift_finder = LiftFinder()
 
 def main():
-    print('hiya')
     img = lift_finder.get_img()
     for i in range(0, 100000):
-        print(i)
         lift_finder.process_image(img)
     lift_finder.send_img()
 
